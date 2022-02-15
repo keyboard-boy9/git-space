a932bc060cb4afff8bc69043dddd0c9a00af11fc
keyboard-boy9 <1634304584@qq.com>
2022-02-16 05:44:44 +0800

    向北航行！

--- hello.c
+++ hello.c
@@ -1,11 +1,7 @@
-hello keyboard-boy9!!!!!!!
-hello keyboard-boy9!!!!!!!
-hello keyboard-boy9!!!!!!!
-hello keyboard-boy9!!!!!!!
-hello keyboard-boy9!!!!!!!
-hello keyboard-boy9!!!!!!!
-hello keyboard-boy9!!!!!!!
-hello keyboard-boy9!!!!!!!
-hello keyboard-boy9!!!!!!!
-hello keyboard-boy9!!!!!!!
-hello keyboard-boy9!!!!!!!
+#include <stdio.h>
+
+int main()
+{
+	printf("hello world!\n");
+	return 0;
+}
